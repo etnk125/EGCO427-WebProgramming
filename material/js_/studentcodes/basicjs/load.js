92e9d9d6bdd16ffeef09dd3f6d0cf67b94adc184
etnk125 <94974562+etnk125@users.noreply.github.com>
2022-01-24 09:15:43 +0700

    change name

--- material/js_/studentcodes/basicjs/load.js
+++
@@ -1,8 +0,0 @@
-// load.js
-//   An example to illustrate the load event
-
-// The onload event handler
-
-function load_greeting() {
-
-}
--- material/js_/studentcodes/basicjs/pswd_chkr.js
+++
@@ -1,6 +0,0 @@
-// pswd_chkr.js
-//   Register the event handlers for pswd_chk.html
-     
-document.getElementById("second").onblur = chkPasswords; 
-document.getElementById("myForm").onsubmit = chkPasswords;
-
