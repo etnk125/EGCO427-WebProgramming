238341298ee93a25ba2899162caedeeb83bbc010
etnk125 <94974562+etnk125@users.noreply.github.com>
2022-03-29 03:19:03 +0700

    fix: using components

--- week10/basic-vue/src/App.vue
+++ week10/basic-vue/src/App.vue
@@ -1,7 +1,15 @@
-<script setup>
+<script>
 import Learn1 from "./components/Learn1.vue";
 import Learn2 from "./components/Learn2.vue";
 import Titlecase from "./components/Title.vue";
+export default {
+  name: "App",
+  components: {
+    Learn1,
+    Learn2,
+    Titlecase,
+  },
+};
 </script>
 
 <template>
